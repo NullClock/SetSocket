0d212c007b29dd9e3a457e5f58adf1022410d589
NullClock <146401816+NullClock@users.noreply.github.com>
2023-12-15 02:37:52 -0500

    Create extension.js

---
+++ js/extension.js
@@ -0,0 +1,76 @@
+(function(Scratch) {
+  let _ws;
+  let _wsMessage;
+  
+  class SetSocket {
+    getInfo() {
+      return {
+        id: 'setsocket',
+        name: 'SetSocket',
+        blocks: [
+          {
+            opcode: 'ws_connect',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Connect to WebSocket server: [URL]',
+            arguments: {
+              URL: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "wss://ws.postman-echo.com/raw"
+              }
+            }
+          },
+          {
+            opcode: 'send',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'Send [DATA] to WebSocket',
+            arguments: {
+              DATA: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "Hello, WebSockets!"
+              }
+            }
+          },
+          {
+            opcode: 'whenConnectedToWebSocket',
+            blockType: Scratch.BlockType.EVENT,
+            text: 'When WebSocket connected',
+            isEdgeActivated: false
+          },
+          {
+            opcode: 'whenMessageRecieved',
+            blockType: Scratch.BlockType.EVENT,
+            text: 'When WebSocket Message Recieved',
+            isEdgeActivated: false
+          },
+          {
+            opcode: 'getMessage',
+            blockType: Scratch.BlockType.REPORTED,
+            text: 'message text'
+          }
+        ]
+      }
+    }
+  
+    ws_connect(args) {
+      _ws = new WebSocket(args.URL);
+    }
+  
+    send(args) {
+      _ws.send(args.DATA);
+    }
+
+    getMessage() {
+      return _wsMessage;
+    }
+  }
+
+  _ws.addEventListener("open", () => {
+    Scratch.vm.runtime.startHats("setsocket_whenConnectedToWebSocket");
+  });
+
+  _ws.addEventListener("message", (e) => {
+    const data = e.data;
+
+    _wsMessage = data;
+  });
+})(Scratch);
